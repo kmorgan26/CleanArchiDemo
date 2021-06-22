8bbd46205f9328199ee8d9e5e38809d54283b89f
Kevin Morgan <kmorgan26@gmail.com>
2021-06-22 10:54:28 -0500

    Adding Domain.Core and Implementation of InMemoryBus

--- CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
+++ CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
 
 namespace CleanArch.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediatr;
 
+        public InMemoryBus(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediatr.Send(command);
+        }
     }
 }
